9b31d52853e8701986576d5c712b01e570767bc9
Rafael Olmedo <RAFA-OLMEDO@hotmail.com>
2019-01-12 10:37:38 -0200

    Incluído boostrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import './assets/css/bootstrap.scss'
+import './assets/js/bootstrap'
+
 ReactDom.render(
     <h1>Tiny Blog</h1>,
     document.getElementById('root')
