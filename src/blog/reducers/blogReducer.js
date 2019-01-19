9448e4b9293622111c978ed3d9e511ab1fe1a8a0
Rafael Olmedo <RAFA-OLMEDO@hotmail.com>
2019-01-19 09:23:33 -0200

    Adiciona case do FIELD_CHANGE

--- src/blog/reducers/blogReducer.js
+++ src/blog/reducers/blogReducer.js
@@ -3,6 +3,8 @@ import { SAVE_POST } from "../actions/blogActions"
 
 export const blogReducer = (state= INITIAL_STATE, action) => {
     switch (action.type) {
+        case FIELD_CHANGE:
+            return state;        
         case SAVE_POST:
             return state;
         default:
